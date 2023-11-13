3b18b49cf8c6ad97177d706ec2411e3fecbff660
aristrisnawan <aristrisnawan35@gmail.com>
2023-11-13 23:43:56 +0700

    show data top anime

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,29 @@
+import AnimeList from "@/app/components/AnimeList";
 
-export default function Home() {
+const page = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`,
+    { cache: "no-store" }
+  );
+  const anime = await response.json();
+  console.log(anime);
   return (
-    <h1>ANIMELIST</h1>
-  )
-}
+    <div>
+      <h1>Paling Populer</h1>
+      <div className=" grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+        {anime.data.map((datas, idx) => {
+          return (
+            <div key={idx} className=" shadow-xl">
+              <AnimeList
+                title={datas.title}
+                images={datas.images.webp.image_url}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default page;
