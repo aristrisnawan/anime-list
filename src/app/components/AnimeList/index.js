db35c4b7bbd21c6f56217437218e6d0f55244609
aristrisnawan <aristrisnawan35@gmail.com>
2023-11-15 23:21:43 +0700

    change ui theme

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -5,9 +5,9 @@ import React from "react";
 const AnimeList = ({ api }) => {
   return (
     <div className=" grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
-      {api.data.map((anime, idx) => {
+      {api?.data?.map((anime, idx) => {
         return (
-          <Link href={`/${anime.mal_id}`} key={idx} className=" cursor-pointer">
+          <Link href={`/${anime.mal_id}`} key={idx} className=" cursor-pointer text-color-primary hover:text-color-accent transition-all">
             <Image
               src={anime.images.webp.image_url}
               alt="..."
@@ -15,7 +15,7 @@ const AnimeList = ({ api }) => {
               height={350}
               className="w-full max-h-64 object-cover"
             />
-            <h3 className=" font-bold md:text-xl p-4 text-md">{anime.title}</h3>
+            <h3 className=" font-bold md:text-xl p-4 text-md ">{anime.title}</h3>
           </Link>
         );
       })}
---
+++ src/app/loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return  (
+    <div className=' flex justify-center items-center min-h-screen'>
+        <div class="flex justify-center items-center loading"></div>
+    </div>
+  )
+  
+}
+
+export default Loading
